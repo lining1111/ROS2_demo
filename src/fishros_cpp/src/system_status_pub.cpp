65341815c67425887f82d3fa4f20c54b467563f7
lining1111 <770677655@qq.com>
2024-12-03 17:17:42 +0800

    跟着小鱼学ros2

--- src/fishros_cpp/src/system_status_pub.cpp
+++ src/fishros_cpp/src/system_status_pub.cpp
@@ -4,6 +4,27 @@
 #include "rclcpp/rclcpp.hpp"
 #include "fishros_cpp/msg/system_status.hpp"
 
+class SysStatusPub:public rclcpp::Node{
+private:
+    rclcpp::Publisher<fishros_cpp::msg::SystemStatus>::SharedPtr pub;
+    rclcpp::TimerBase::SharedPtr timer;
+
+    void timer_callback(){
+        fishros_cpp::msg::SystemStatus msg;
+
+        RCLCPP_INFO(this->get_logger(),"SysStatusPub pub "+ msg.host);
+        pub->publish(msg);
+    }
+
+public:
+    SysStatusPub():Node("SysStatusPub"){
+        RCLCPP_INFO(this->get_logger(),"SysStatusPub 启动");
+        pub = create_publisher<fishros_cpp::msg::SystemStatus>("Novel",5);
+        timer = create_wall_timer(std::chrono::seconds(5),std::bind(&SysStatusPub::timer_callback,this));
+    }
+
+
+};
 
 int main(int argc,char **argv){
     rclcpp::init(argc,argv);
